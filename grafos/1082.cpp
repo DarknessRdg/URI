e4ddb14463ba3fb4a565a69e14d3fa668ebbffaa
Luan Rodrigues <38518014+DarknessRdg@users.noreply.github.com>
2021-10-28 02:34:41 -0300

    Componentes Conexos - Optimize

--- grafos/1082.cpp
+++ grafos/1082.cpp
@@ -4,14 +4,23 @@
 #include <algorithm>
 using namespace std;
 
+class Node;
+class Edge;
+
+
 class Node {
 public:
     char rep;
     int visited;
+    vector<Edge*> edges;
 
     Node(char representation) {
         rep = representation;
         visited = false;
+    }
+
+    void add_edge(Edge* e) {
+        edges.emplace_back(e);
     }
 
     bool equal(Node* other) {
@@ -32,6 +41,9 @@ public:
         a = _a;
         b = _b;
         visited = false;
+
+        a->add_edge(this);
+        b->add_edge(this);
     }
 
     bool is_available(Node* node) {
@@ -49,11 +61,7 @@ public:
 };
 
 
-vector<char> bfs(
-        Edge* edges[],
-        int amount_edges,
-        Node* current_node
-) {
+vector<char> bfs(Node* current_node) {
     vector<char> components;
 
     stack<Node*> nodes_stack;
@@ -70,9 +78,7 @@ vector<char> bfs(
         current_node->visit();
         components.emplace_back(current_node->rep);
 
-        for (int i = 0; i < amount_edges; i++) {
-            auto e = edges[i];
-
+        for (auto e: current_node->edges) {
             if (e->is_available(current_node)) {
                 e->visit();
                 nodes_stack.push(e->get_different_node(current_node));
@@ -113,7 +119,8 @@ void test_case() {
         auto node = nodes[i];
 
         if (!node->visited) {
-            auto components = bfs(edges, amount_edges, node);
+            auto components = bfs(node);
+
             sort(components.begin(), components.end());
 
             for (auto c: components) {
